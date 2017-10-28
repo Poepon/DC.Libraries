2d15447d76b277cbfecd7e2242d80af747f81fbc
Dell <Dell@PC-Admin>
2017-10-28 11:18:38 +0800

    修改引用的System.Drawing DLL。

--- src/DC.Libraries.Extensions.Captcha/Captcha/Providers/CaptchaImageProvider.cs
+++ src/DC.Libraries.Extensions.Captcha/Captcha/Providers/CaptchaImageProvider.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Drawing;
 using System.IO;
 using DC.Libraries.Extensions.Captcha.Contracts;
-using System.Drawing.Imaging;
+using System.DrawingCore;
+using System.DrawingCore.Imaging;
 
 namespace DC.Libraries.Extensions.Captcha.Providers
 {
